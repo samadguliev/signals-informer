11c156d6b4793e440cabd12fa956839b495adef0
Samad Guliev <samadguliev@mail.ru>
2024-03-18 23:30:22 +0300

    Env file

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
+import os
 import discord
+from dotenv import load_dotenv
 from discord.ext import commands
 
+load_dotenv()
 
 def run_discord_bot():
     intents = discord.Intents.default()
     intents.message_content = True
 
-    TOKEN = ""
+    TOKEN = os.getenv("TOKEN")
     client = commands.Bot(command_prefix="/", intents=intents)
 
     @client.event
