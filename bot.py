d477615ab43cb25a446c38bb0377af557fd42deb
Samad Guliev <samadguliev@mail.ru>
2024-08-02 23:11:40 +0300

    Signal model, table init

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 from logger import logger
+from models import Signal
 
 load_dotenv()
 
@@ -14,18 +15,54 @@ def run_discord_bot():
     TOKEN = os.getenv("TOKEN")
     client = commands.Bot(command_prefix="/", intents=intents)
 
+    async def get_pinned_messages() -> list[Signal]:
+        return Signal.get_all()
+
+    async def init_table(interaction: discord.Interaction):
+        try:
+            Signal.clear_table()
+            logger.info(f"Table is empty")
+
+            pinned_messages = await interaction.channel.pins()
+            if len(pinned_messages) == 0:
+                return
+
+            messages: list[str] = []
+            for msg in pinned_messages:
+                msg_content = msg.content
+
+                try:
+                    index = msg_content.index("\n")
+                except ValueError:
+                    index = None
+
+                formatted_msg = msg_content[:msg_content.index("\n")] + "\n" if index else msg_content
+                formatted_msg = formatted_msg.replace("*️⃣", ":asterisk:")
+                messages.append(formatted_msg)
+
+            Signal.create_signals(messages)
+
+        except Exception as e:
+            logger.error(e)
+
     @client.event
     async def on_ready():
         try:
             synced = await client.tree.sync()
             logger.info(f"Synced {len(synced)} command(s)")
         except Exception as e:
-            print(e)
+            logger.error(e)
 
     @client.tree.command(name="signals_info")
-    async def signals_info(interaction: discord.Interaction):
+    async def signals_info(interaction: discord.Interaction, option: str = None):
         await interaction.response.defer(ephemeral=True)
-        pinned_messages = await interaction.channel.pins()
+
+        if option == "-init":
+            await init_table(interaction)
+            await interaction.followup.send("Success")
+            return
+
+        pinned_messages = await get_pinned_messages()
 
         if len(pinned_messages) == 0:
             await interaction.followup.send("There are no pinned messages in this channel")
@@ -35,28 +72,19 @@ def run_discord_bot():
         embed_list = []
 
         for msg in pinned_messages:
-            msg_content = msg.content
-
-            try:
-                index = msg_content.index("\n")
-            except ValueError:
-                index = None
-
-            formatted_msg = msg_content[:msg_content.index("\n")] + "\n" if index else msg_content + "\n"
-            formatted_msg = formatted_msg.replace("*️⃣", ":asterisk:")
-
-            if (len(signals_message) + len(formatted_msg)) < 1000:
-                signals_message += formatted_msg
+            if (len(signals_message) + len(msg.text)) < 1000:
+                signals_message += msg.text
                 continue
 
             embed_list.append(signals_message)
             signals_message = ""
-            signals_message += formatted_msg
+            signals_message += msg.text
 
         if len(signals_message) > 0:
             embed_list.append(signals_message)
 
-        embed_var = discord.Embed(title="Pinned signals", description=f"Total count: {len(pinned_messages)}", color=0x00ff00)
+        embed_var = discord.Embed(title="Pinned signals", description=f"Total count: {len(pinned_messages)}",
+                                  color=0x00ff00)
 
         count = 1
         for val in embed_list:
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from peewee import Model, CharField, IntegerField, ForeignKeyField
+import datetime
+from logger import logger
+from peewee import Model, CharField, IntegerField, DateTimeField, AutoField
 from database import db, get_db_connection
 
 
@@ -16,3 +18,40 @@ class Test(BaseModel):
         with get_db_connection():
             user = Test.create(username=username, age=age)
             return user
+
+
+class Signal(BaseModel):
+    id = AutoField()
+    text = CharField()
+    created = DateTimeField(default=datetime.datetime.now)
+
+    @staticmethod
+    def create_signals(signals) -> list['Signal']:
+        with get_db_connection():
+            with db.atomic():
+                try:
+                    signals_list: list[Signal] = [Signal(text=signal) for signal in signals]
+                    Signal.bulk_create(signals_list)
+                    return signals_list
+                except BaseException as e:
+                    logger.error(str(e))
+                    return []
+
+    @staticmethod
+    def clear_table():
+        with get_db_connection():
+            with db.atomic():
+                try:
+                    delete_q = Signal.delete()
+                    delete_q.execute()
+                except BaseException as e:
+                    logger.error(str(e))
+
+    @staticmethod
+    def get_all() -> list['Signal']:
+        with get_db_connection():
+            with db.atomic():
+                try:
+                    return Signal.select()
+                except BaseException as e:
+                    logger.error(str(e))
